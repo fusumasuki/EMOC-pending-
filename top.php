9c47d427d5d5570546bbef976f64e762b4b0c0ce
Detsudetsu <tubopi311@gmail.com>
2018-12-24 20:13:46 +0900

    top.phpを整形しました

--- top.php
+++ top.php
@@ -1,5 +1,5 @@
 <?php
-session_start();  
+session_start();
 ?>
 
 <html>
@@ -7,39 +7,42 @@ session_start();
         <meta charset="UTF-8">
     </head>
     <body>
-            <h1> 映画リスト </h1>
-            <table>
-            <tr>
-            <td>タイトル</td>
-            <td><b>ワクワク度 </b></td>
-            <td><b>ほっこり度</b></td>
-            <td><b>ドキドキ度</b></td>
-            <td><b>しょんぼり度</b></td>
-            <td><b>イライラ度</b></td></tr>
+        <h1> 映画リスト </h1>
+        <table>
+        <tr>
+        <td>タイトル</td>
+        <td><b>ワクワク度 </b></td>
+        <td><b>ほっこり度</b></td>
+        <td><b>ドキドキ度</b></td>
+        <td><b>しょんぼり度</b></td>
+        <td><b>イライラ度</b></td></tr>
 
-            <?php
-                function h($str) { return htmlspecialchars($str, ENT_QUOTES, "UTF-8"); }
-            $db = new PDO("sqlite:movie.sqlite");
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-            $result=$db->prepare("SELECT * FROM evaluation where user_id=?");
-            $result->execute(array($_SESSION["id"]));
-                for($i = 0; $row=$result->fetch(); ++$i ){  
-                    $title=$db->prepare("SELECT * FROM movie where id = ?");
+        <?php
+        function h($str)
+        {
+            return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+        }
+        $db = new PDO("sqlite:movie.sqlite");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        $result = $db->prepare("SELECT * FROM evaluation where user_id=?");
+        $result->execute(array($_SESSION["id"]));
+        for ($i = 0; $row = $result->fetch(); ++$i) {
+            $title = $db->prepare("SELECT * FROM movie where id = ?");
             $title->execute(array($row['movie_id']));
-                for($i = 0; $row2=$title->fetch(); ++$i ){          
-                    echo "<tr>";
-                    echo "<td>". h($row2['title']). "</td>";
-                    echo "<td>". h($row['excite']). "</td>";
-                    echo "<td>". h($row['relax']). "</td>";
-                    echo "<td>". h($row['fear']). "</td>";
-                    echo "<td>". h($row['sad']). "</td>";
-                    echo "<td>". h($row['anger']). "</td>";
-                    echo "</tr>";
+            for ($i = 0; $row2 = $title->fetch(); ++$i) {
+                echo "<tr>";
+                echo "<td>" . h($row2['title']) . "</td>";
+                echo "<td>" . h($row['excite']) . "</td>";
+                echo "<td>" . h($row['relax']) . "</td>";
+                echo "<td>" . h($row['fear']) . "</td>";
+                echo "<td>" . h($row['sad']) . "</td>";
+                echo "<td>" . h($row['anger']) . "</td>";
+                echo "</tr>";
             }
         }
-            ?>
-            </table>   
-            <a href="search.php">記事投稿</a>          
-            </body>
-            </html>
+        ?>
+        </table>   
+        <a href="search.php">記事投稿</a>          
+    </body>
+</html>
             
