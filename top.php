eab0dbddb9b1bfbc45cdd9b5a29565a943e74b7a
Daigo Tsubouchi <36166146+Detsudetsu@users.noreply.github.com>
2018-12-24 18:23:13 +0900

    Add files via upload

---
+++ top.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+            <h1> 映画リスト </h1>
+            <table>
+            <tr>
+            <td>タイトル</td>
+            <td><b>ワクワク度 </b></td>
+            <td><b>ほっこり度</b></td>
+            <td><b>ドキドキ度</b></td>
+            <td><b>しょんぼり度</b></td>
+            <td><b>イライラ度</b></td></tr>
+
+            <?php
+                function h($str) { return htmlspecialchars($str, ENT_QUOTES, "UTF-8"); }
+            $db = new PDO("sqlite:movie.sqlite");
+            $result=$db->query("SELECT * FROM evaluation where user_id=1");
+                for($i = 0; $row=$result->fetch(); ++$i ){  
+                    $title=$db->prepare("SELECT * FROM movie where id = ?");
+            $title->execute(array($row['movie_id']));
+                for($i = 0; $row2=$title->fetch(); ++$i ){           
+                    echo "<tr>";
+                    echo "<td>". h($row2['title']). "</td>";
+                    echo "<td>". h($row['excite']). "</td>";
+                    echo "<td>". h($row['relax']). "</td>";
+                    echo "<td>". h($row['fear']). "</td>";
+                    echo "<td>". h($row['sad']). "</td>";
+                    echo "<td>". h($row['anger']). "</td>";
+                    echo "</tr>";
+            }
+        }
+            ?>
+            </table>             
+            </body>
+            </html>
+            
